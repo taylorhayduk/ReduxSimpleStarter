c511eddc80c9d01f91932cd759121c1a6ea6ad0b
Taylor Hayduk <taylorhayduk@gmail.com>
2016-01-28 12:45:39 -0500

    Index.js and component stubs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
+// Use babel.io to see ES6 + JSX >> Vanilla ES5
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component.  Should produce HTML
+const App = function () { // const is es6 variable that cant change
+  return <div>Hi!</div>;  // This is JSX (javascript dialect that gets transpiled by Webpack)
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Throw produced HTML into the page 
+ReactDOM.render(<App />, document.querySelector('.container'));  
+  // <App /> is the same as <App></App>.  Makes it an instance of the class App
+  // 2nd argument is where to put the rendered App
